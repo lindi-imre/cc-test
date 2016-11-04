bd6b2228136737473460916093aa7348abf14903
lindi-imre <lindi.imre@gmail.com>
2016-11-04 12:45:23 +0100

    message

--- read_file.py
+++ read_file.py
@@ -1,17 +1,8 @@
 my_file = open("testfile.txt", "r")
-
-for line in my_file:
-    print(line.strip())
-
+output_list = []
+for i in my_file:
+    output_list.append(int(i) + 100)
 my_file.close()
-
-my_list = []
-my_list.append("elso")
-my_list.append("masodik")
-my_list.append("harmadik")
-
 my_file = open("testfile.txt", "w")
-
-for i in my_list:
-    my_file.write(i + "\n")
-my_file.write("itt a vege")
+for i in output_list:
+    my_file.write(str(i) + "\n")
